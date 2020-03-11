55a84b045791cef820471083187bc3847fab21dd
LosVM <los.vladimir@mail.ru>
2020-03-11 19:34:57 +0300

    321

--- js/script.js
+++ js/script.js
@@ -3,12 +3,13 @@
 var popup = document.querySelector(".modal-hide");
 var popup = document.querySelector(".modal-show");
 var popup = document.querySelector(".modal-search-form");
+var button = document.querySelector(".hotels-search-button");
 
 if (popup) {
-    popup.classList.toggle("modal-show");
+    popup.classList.toggle("modal-hide");
 }
 
 button.addEventListener("click", function(evt) {
     evt.preventDefault();
-    popup.classlist.toggle("modal-show");    
+    popup.classlist.toggle("modal-hide");    
 });
