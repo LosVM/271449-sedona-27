fe0cc11b38430bcaef5a956d958e7732f6c1a5b1
LosVM <los.vladimir@mail.ru>
2020-03-23 23:11:22 +0300

    312

--- js/hotels-searc.js
+++ js/hotels-searc.js
@@ -1,15 +1,14 @@
-
- 
-var popup = document.querySelector(".modal-hide");
-var popup = document.querySelector(".modal-show");
-var popup = document.querySelector(".modal-search-form");
-var button = document.querySelector(".hotels-search-button");
-
+var popup = document.querySelector(".form-main"); 
+   
 if (popup) {
-    popup.classList.toggle("modal-show");
+  popup.classList.toggle("modal-show");
 }
 
+var button = document.querySelector(".hotels-search-button");
+var form = popup.querySelector("modal-search-form");
+
 button.addEventListener("click", function (evt) {
+    
     evt.preventDefault();
-    popup.classList.toggle("modal-show");    
+    popup.classList.toggle("modal-show");        
 });
